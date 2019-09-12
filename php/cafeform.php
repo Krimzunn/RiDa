a5f19eb3baa68363b4cb8c2745c5ce033914f931
Krimzunn <ryzarprozar1632@gmail.com>
2019-09-12 10:09:21 +1000

    YEAAAAAAAHHHHHHHHHHHHHHHh

---
+++ php/cafeform.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+First Name is  <?php echo $_GET["name"]; ?><br>
+Days Attended was  <?php echo $_GET["days"]; ?><br>
+Campus is  <?php echo $_GET["campus"]; ?><br>
+Quality was <?php echo $_GET["quality"]; ?> <br>
+</body>
+</html>
