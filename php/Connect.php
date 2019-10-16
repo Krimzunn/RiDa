881bafe4e19ec04cd0b3ccff8ccfef6981f722fc
Krimzunn <ryzarprozar1632@gmail.com>
2019-10-17 09:57:10 +1100

    Task 11

--- php/Connect.php
+++ php/Connect.php
@@ -2,15 +2,31 @@
 $servername = "localhost";
 $username = "rida";
 $password = "Password1";
+$my_db = "Task";
+
+
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password);
+$conn = mysqli_connect($servername, $username, $password, $my_db);
 
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 echo "Connected successfully";
+
+
+
+
+// Create table
+$sql="CREATE TABLE Persons(FirstName CHAR(30),LastName CHAR(30),Age INT)";
+// Execute query
+if (mysqli_query($conn,$sql)) {
+    echo "Table persons created successfully";
+} else {
+    echo "Error creating table: " . mysqli_error($con);
+}
+
 ?>
 <html>
 <body>
